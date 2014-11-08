14b07d17b0b88684429222cb278bf4a4a2037e04
Scott Crawford <scottcrawford03@gmail.com>
2014-11-08 16:30:36 -0700

    got the guess class to pass tests. still need to refactor

--- lib/guess.rb
+++ lib/guess.rb
@@ -1,21 +1,23 @@
 class Guess
-  attr_reader :guess
-              :guess_count
-              :previous_guess
+  attr_reader   :guess           # => nil
+  attr_accessor :guess_count     # => nil
+                :previous_guess  # => :previous_guess
+
   def initialize
-    @guess = []
-    @guess_count = 0
-    @previous_guess = []
+    @guess = []           # => []
+    @guess_count = 0      # => 0
+    @previous_guess = []  # => []
   end
 
   def user_guess(word)
-    guess = []
-    letters = word.split(//)
+    guess = []                                       # => []
+    letters = word.split(//)                         # => ["r", "r", "r", "r"]
     case
     when letters.size >= 5
       guess_again
     else
-      guess << letters && previous_guess << letters
+      @guess_count += 1                              # => 1
+      guess << letters && @previous_guess << letters  # ~> NameError: undefined local variable or method `previous_guess' for #<Guess:0x007fed41871488>
     end
   end
 
@@ -23,14 +25,30 @@ class Guess
     "You may only guess 4 colors."
   end
 
-  def previous_guess
-    @previous_guess
+  def guess_count
+    case
+    when @guess_count < 2
+      "You have taken #{@guess_count} guess"
+    else
+      "You have taken #{@guess_count} guesses."
+    end
+  end
+
+  def last_guess
+    @previous_guess.join('')
   end
 
 end
 
-new_guess = Guess.new
-new_guess.user_guess("rggg")
-new_guess.previous_guess
+new_guess = Guess.new         # => #<Guess:0x007fed41871488 @guess=[], @guess_count=0, @previous_guess=[]>
 new_guess.user_guess("rrrr")
-new_guess.previous_guess
+new_guess.guess_count
+new_guess.user_guess("rrrr")
+new_guess.guess_count
+new_guess.last_guess
+
+# ~> NameError
+# ~> undefined local variable or method `previous_guess' for #<Guess:0x007fed41871488>
+# ~>
+# ~> /Users/scottcrawford/Turing/mastermind/lib/guess.rb:20:in `user_guess'
+# ~> /Users/scottcrawford/Turing/mastermind/lib/guess.rb:44:in `<main>'
--- test/guess_test.rb
+++ test/guess_test.rb
@@ -1,7 +1,7 @@
-gem 'minitest', '~> 5.2'
-require 'minitest/autorun'
-require 'minitest/pride'
-require_relative '../lib/guess'
+gem 'minitest', '~> 5.2'         # => true
+require 'minitest/autorun'       # => true
+require 'minitest/pride'         # => true
+require_relative '../lib/guess'  # ~> TypeError: no implicit conversion of Array into String
 
 class GuessTest < Minitest::Test
 
@@ -17,23 +17,40 @@ class GuessTest < Minitest::Test
 
   def test_that_after_guess_array_is_empty_again
     new_guess = Guess.new
-    assert guess.empty?
-    new_guess.guess("rgby")
-    assert_equal 4, new_guess.guess.length
-    assert guess.empty?
+    assert new_guess.guess.empty?
+    new_guess.user_guess("rgby")
+    assert_equal 0, new_guess.guess.length
   end
 
   def test_that_previous_guess_gets_stored
     new_guess = Guess.new
-    new_guess.guess("rrrr")
-    assert_equal "rrrr", new_guess.previous_guess
+    new_guess.user_guess("rrrr")
+    assert_equal "rrrr", new_guess.last_guess
   end
 
   def test_that_it_counts_the_number_guesses
     new_guess = Guess.new
-    new_guess.guess("rrrr")
-    new_guess.guess("rrrr")
-    new_guess.guess("rrrr")
+    new_guess.user_guess("rrrr")
+    new_guess.user_guess("rrrr")
+    new_guess.user_guess("rrrr")
     assert_equal "You have taken 3 guesses.", new_guess.guess_count
   end
 end
+
+# >> Run options: --seed 57433
+# >>
+# >> # Running:
+# >>
+# >>
+# >>
+# >> [38;5;154mF[0m[38;5;154ma[0m[38;5;148mb[0m[38;5;184mu[0m[38;5;184ml[0m[38;5;214mo[0m[38;5;214mu[0m[38;5;208ms[0m[38;5;208m [0m[38;5;203mr[0m[38;5;203mu[0m[38;5;198mn[0m in 0.001781s, 0.0000 runs/s, 0.0000 assertions/s.
+# >>
+# >> 0 runs, 0 assertions, 0 failures, 0 errors, 0 skips
+
+# ~> TypeError
+# ~> no implicit conversion of Array into String
+# ~>
+# ~> /Users/scottcrawford/Turing/mastermind/lib/guess.rb:20:in `user_guess'
+# ~> /Users/scottcrawford/Turing/mastermind/lib/guess.rb:45:in `<top (required)>'
+# ~> /Users/scottcrawford/Turing/mastermind/test/guess_test.rb:4:in `require_relative'
+# ~> /Users/scottcrawford/Turing/mastermind/test/guess_test.rb:4:in `<main>'
