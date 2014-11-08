028159a4a352301a74cfcff5b179859056c7aaf0
Scott Crawford <scottcrawford03@gmail.com>
2014-11-08 16:34:34 -0700

    took of SIB in guess class

--- lib/guess.rb
+++ lib/guess.rb
@@ -1,24 +1,24 @@
 class Guess
-  attr_reader   :guess           # => nil
-  attr_accessor :guess_count     # => nil
-                :previous_guess  # => :previous_guess
+  attr_reader   :guess
+  attr_accessor :guess_count
+                :previous_guess
 
   def initialize
-    @guess = []           # => []
-    @guess_count = 0      # => 0
-    @previous_guess = []  # => []
+    @guess = []
+    @guess_count = 0
+    @previous_guess = []
   end
 
   def user_guess(word)
-    guess = []                                        # => [], []
-    letters = word.split(//)                          # => ["r", "r", "r", "r"], ["r", "r", "r", "r"]
+    guess = []
+    letters = word.split(//)
     case
     when letters.size >= 5
       guess_again
     else
-      @guess_count += 1                               # => 1, 2
-      guess << letters && @previous_guess << letters  # => [["r", "r", "r", "r"]], [["r", "r", "r", "r"], ["r", "r", "r", "r"]]
-    end                                               # => [["r", "r", "r", "r"]], [["r", "r", "r", "r"], ["r", "r", "r", "r"]]
+      @guess_count += 1
+      guess << letters && @previous_guess << letters
+    end
   end
 
   def guess_again
@@ -28,22 +28,22 @@ class Guess
   def guess_count
     case
     when @guess_count < 2
-      "You have taken #{@guess_count} guess"     # => "You have taken 1 guess"
+      "You have taken #{@guess_count} guess"
     else
-      "You have taken #{@guess_count} guesses."  # => "You have taken 2 guesses."
-    end                                          # => "You have taken 1 guess", "You have taken 2 guesses."
+      "You have taken #{@guess_count} guesses."
+    end
   end
 
   def last_guess
-    last = @previous_guess.join('')  # => "rrrrrrrr"
-    last[-4..-1]                     # => "rrrr"
+    last = @previous_guess.join('')
+    last[-4..-1]
   end
 
 end
 
-new_guess = Guess.new         # => #<Guess:0x007fc614235208 @guess=[], @guess_count=0, @previous_guess=[]>
-new_guess.user_guess("rrrr")  # => [["r", "r", "r", "r"]]
-new_guess.guess_count         # => "You have taken 1 guess"
-new_guess.user_guess("rrrr")  # => [["r", "r", "r", "r"], ["r", "r", "r", "r"]]
-new_guess.guess_count         # => "You have taken 2 guesses."
-new_guess.last_guess          # => "rrrr"
+new_guess = Guess.new
+new_guess.user_guess("rrrr")
+new_guess.guess_count
+new_guess.user_guess("rrrr")
+new_guess.guess_count
+new_guess.last_guess
--- test/guess_test.rb
+++ test/guess_test.rb
@@ -1,7 +1,7 @@
-gem 'minitest', '~> 5.2'         # => true
-require 'minitest/autorun'       # => true
-require 'minitest/pride'         # => true
-require_relative '../lib/guess'  # ~> TypeError: no implicit conversion of Array into String
+gem 'minitest', '~> 5.2'
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/guess'
 
 class GuessTest < Minitest::Test
 
@@ -36,21 +36,3 @@ class GuessTest < Minitest::Test
     assert_equal "You have taken 3 guesses.", new_guess.guess_count
   end
 end
-
-# >> Run options: --seed 57433
-# >>
-# >> # Running:
-# >>
-# >>
-# >>
-# >> [38;5;154mF[0m[38;5;154ma[0m[38;5;148mb[0m[38;5;184mu[0m[38;5;184ml[0m[38;5;214mo[0m[38;5;214mu[0m[38;5;208ms[0m[38;5;208m [0m[38;5;203mr[0m[38;5;203mu[0m[38;5;198mn[0m in 0.001781s, 0.0000 runs/s, 0.0000 assertions/s.
-# >>
-# >> 0 runs, 0 assertions, 0 failures, 0 errors, 0 skips
-
-# ~> TypeError
-# ~> no implicit conversion of Array into String
-# ~>
-# ~> /Users/scottcrawford/Turing/mastermind/lib/guess.rb:20:in `user_guess'
-# ~> /Users/scottcrawford/Turing/mastermind/lib/guess.rb:45:in `<top (required)>'
-# ~> /Users/scottcrawford/Turing/mastermind/test/guess_test.rb:4:in `require_relative'
-# ~> /Users/scottcrawford/Turing/mastermind/test/guess_test.rb:4:in `<main>'
