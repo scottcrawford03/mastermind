aa2ba5b1e10450dd3b5bce6bc2416bbbffeec97e
Scott Crawford <scottcrawford03@gmail.com>
2014-11-08 12:40:03 -0700

    added gets into game prompts class

--- lib/game_prompts.rb
+++ lib/game_prompts.rb
@@ -4,19 +4,27 @@
 # ask if they want to play again - y for yes, n for no
 
 class GamePrompts
+  attr_reader :intro_message                                                                                 # => nil
+  def initialize
+    @intro_message = "Welcome to MASTERMIND\nWould you like to (p)lay, read the (i)nstructions, or (q)uit?"
 
-  def intro_message
-    "Welcome to MASTERMIND\nWould you like to (p)lay, read the (i)nstructions, or (q)uit?"
+
   end
 
-  def welcome(letter)
+  def welcome
+    puts intro_message
+    welcome_options
+  end
+
+  def welcome_options
+    letter = gets
     case letter
     when "p" || "P"
-      play
+      puts play
     when "i" || "I"
-      instructions
+      puts instructions
     when "q" || "Q"
-      quit
+      puts quit
     else
       invalid
     end
@@ -24,7 +32,7 @@ class GamePrompts
 
   def play
     "begin the game."
-end
+  end
 
   def instructions
     "A secret combination of colors has been chosen at random.\n\nYour job is to guess the correct sequence in 10 tries or less.\n\nIf you manage to win, you'll join the elite as a mastermind.\n\nIf you lose, the shambolic state of your life will be confirmed.\n\nGood luck!...you'll need it."
@@ -37,7 +45,12 @@ end
   def invalid
     "Your argument is invalid. Try again."
   end
+
+  def play_again
+  
+  end
+
 end
 
-game = GamePrompts.new
-game.welcome("I")
+player = GamePrompts.new
+player.welcome
--- test/game_prompts_test.rb
+++ test/game_prompts_test.rb
@@ -14,24 +14,24 @@ class GamePromptsTest < Minitest::Test
 
   def test_that_when_player_hits_p_it_starts_game
     player = GamePrompts.new
-    assert_equal "begin the game.", player.welcome("p")
+    assert_equal "begin the game.", player.play
 
   end
 
   def test_that_when_player_hits_i_it_shows_instructions
     player = GamePrompts.new
-    assert_equal "A secret combination of colors has been chosen at random.\n\nYour job is to guess the correct sequence in 10 tries or less.\n\nIf you manage to win, you'll join the elite as a mastermind.\n\nIf you lose, the shambolic state of your life will be confirmed.\n\nGood luck!...you'll need it." , player.welcome("i")
+    assert_equal "A secret combination of colors has been chosen at random.\n\nYour job is to guess the correct sequence in 10 tries or less.\n\nIf you manage to win, you'll join the elite as a mastermind.\n\nIf you lose, the shambolic state of your life will be confirmed.\n\nGood luck!...you'll need it." , player.instructions
   end
 
   def test_that_when_player_hits_q_it_quits_and_says_farewell_message
     player = GamePrompts.new
-    assert_equal "Your father was right about you, you are a quitter.", player.welcome("q")
+    assert_equal "Your father was right about you, you are a quitter.", player.quit
 
   end
 
   def test_that_any_other_key_brings_invalid_message
     player = GamePrompts.new
-    assert_equal "Your argument is invalid. Try again.", player.welcome("t")
+    assert_equal "Your argument is invalid. Try again.", player.invalid
   end
 
 end
