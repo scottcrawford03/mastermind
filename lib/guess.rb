0a882b793a98e16eb53afdfbe4a9def4fad77a7b
Scott Crawford <scottcrawford03@gmail.com>
2014-11-08 16:33:53 -0700

    last_guess now only shows last guess

--- lib/guess.rb
+++ lib/guess.rb
@@ -10,15 +10,15 @@ class Guess
   end
 
   def user_guess(word)
-    guess = []                                       # => []
-    letters = word.split(//)                         # => ["r", "r", "r", "r"]
+    guess = []                                        # => [], []
+    letters = word.split(//)                          # => ["r", "r", "r", "r"], ["r", "r", "r", "r"]
     case
     when letters.size >= 5
       guess_again
     else
-      @guess_count += 1                              # => 1
-      guess << letters && @previous_guess << letters  # ~> NameError: undefined local variable or method `previous_guess' for #<Guess:0x007fed41871488>
-    end
+      @guess_count += 1                               # => 1, 2
+      guess << letters && @previous_guess << letters  # => [["r", "r", "r", "r"]], [["r", "r", "r", "r"], ["r", "r", "r", "r"]]
+    end                                               # => [["r", "r", "r", "r"]], [["r", "r", "r", "r"], ["r", "r", "r", "r"]]
   end
 
   def guess_again
@@ -28,27 +28,22 @@ class Guess
   def guess_count
     case
     when @guess_count < 2
-      "You have taken #{@guess_count} guess"
+      "You have taken #{@guess_count} guess"     # => "You have taken 1 guess"
     else
-      "You have taken #{@guess_count} guesses."
-    end
+      "You have taken #{@guess_count} guesses."  # => "You have taken 2 guesses."
+    end                                          # => "You have taken 1 guess", "You have taken 2 guesses."
   end
 
   def last_guess
-    @previous_guess.join('')
+    last = @previous_guess.join('')  # => "rrrrrrrr"
+    last[-4..-1]                     # => "rrrr"
   end
 
 end
 
-new_guess = Guess.new         # => #<Guess:0x007fed41871488 @guess=[], @guess_count=0, @previous_guess=[]>
-new_guess.user_guess("rrrr")
-new_guess.guess_count
-new_guess.user_guess("rrrr")
-new_guess.guess_count
-new_guess.last_guess
-
-# ~> NameError
-# ~> undefined local variable or method `previous_guess' for #<Guess:0x007fed41871488>
-# ~>
-# ~> /Users/scottcrawford/Turing/mastermind/lib/guess.rb:20:in `user_guess'
-# ~> /Users/scottcrawford/Turing/mastermind/lib/guess.rb:44:in `<main>'
+new_guess = Guess.new         # => #<Guess:0x007fc614235208 @guess=[], @guess_count=0, @previous_guess=[]>
+new_guess.user_guess("rrrr")  # => [["r", "r", "r", "r"]]
+new_guess.guess_count         # => "You have taken 1 guess"
+new_guess.user_guess("rrrr")  # => [["r", "r", "r", "r"], ["r", "r", "r", "r"]]
+new_guess.guess_count         # => "You have taken 2 guesses."
+new_guess.last_guess          # => "rrrr"
