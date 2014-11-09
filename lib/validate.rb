eb28eba6ab20bfafb5b044446078700a54cd6270
Scott Crawford <scottcrawford03@gmail.com>
2014-11-09 12:13:08 -0700

    wrote validate test and code

--- lib/validate.rb
+++ lib/validate.rb
@@ -1,4 +1,35 @@
-determine how many colors correct
-determine how many positions correct
-verify user guess with answer array
-track number of guesses - guess 1, guess 2, etc. && "you have X number of guess left"
+# determine how many colors correct
+# determine how many positions correct
+# verify user guess with answer array
+# track number of guesses - guess 1, guess 2, etc. && "you have X number of guess left"
+require_relative '../lib/guess'
+require_relative '../lib/colors'
+
+class Validate
+
+  def position_check
+    user_guess.zip(secret_answer).map { |user, answer| user == answer }
+
+  end
+
+  def number_correct
+  compared_guess =  user_guess - secret_answer
+  correct = 4 - compared_guess.size
+  correct
+  end
+
+  def after_guess
+    "You have #{number_correct} correct colors and #{position_check} in the right position."
+  end
+
+
+  def correct?
+    case
+    when user_guess == secret_answer
+      "You win!"
+    else
+      puts position_check
+      puts number_correct
+    end
+  end
+end
--- test/validate_test.rb
+++ test/validate_test.rb
@@ -1,11 +1,13 @@
 gem 'minitest', '~> 5.2'
 require 'minitest/autorun'
 require 'minitest/pride'
-require 'validate'
+require_relative '../lib/validate'
+require_relative '../lib/guess'
 
 class ValidateTest < Minitest::Test
 
   def test_that_it_checks_guess_against_answer
+
     assert secret_answer == guess
   end
 
@@ -15,6 +17,7 @@ class ValidateTest < Minitest::Test
 
   def test_that_it_says_how_many_colors_are_in_the_right_position
     assert_equal 2, player.guess('rrgb')
+
   end
 
 
